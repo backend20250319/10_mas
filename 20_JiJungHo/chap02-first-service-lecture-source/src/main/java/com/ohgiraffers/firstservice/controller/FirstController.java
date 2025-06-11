28af546bd8441312c482541d0766c35eaf7263f2
jungho <kiik52@naver.com>
2025-06-11 10:26:21 +0900

    chap02 - first-service, second-service - gateway 테스트를 위한 yml파일 수정

--- 20_JiJungHo/chap02-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/controller/FirstController.java
+++ 20_JiJungHo/chap02-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/controller/FirstController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/first-service")
+//@RequestMapping("/first-service")
 public class FirstController {
     private Environment environment;
 
